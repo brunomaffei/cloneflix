85ae43ec76034b1f7b0bf1cc88b19dd5ce79fd88
Bruno Maffei <bruno.maffei@hotmail.com>
2021-08-18 19:26:44 -0300

    fixing variables to gitignore

--- src/api/Tmdb.js
+++ src/api/Tmdb.js
@@ -1,8 +1,3 @@
-// Aqui nós estamos pegando as informações para bater na API.
-
-const API_KEY = "74bbf1917dbd2a4aed978f3c6923b3bb"; // Adicionar API KEY FROM TMDB
-const API_BASE = "https://api.themoviedb.org/3";
-
 /* 
 - originais da netflix
 - recomendados (trending)
@@ -14,9 +9,11 @@ const API_BASE = "https://api.themoviedb.org/3";
 - documentários
 */
 
+const baseURL = "https://api.themoviedb.org/3";
+
 const basicFetch = async (endpoint) => {
-  const req = await fetch(`${API_BASE}${endpoint}`);
-  const json = await req.json();
+  const req = await fetch(`${baseURL}${endpoint}`);
+  const json = req.json();
   return json;
 };
 
@@ -27,56 +24,56 @@ const exportedObjetc = {
         slug: "originals",
         title: "Originais do Netflix",
         items: await basicFetch(
-          `/discover/tv/?with_network=213&language=pt-BR&api_key=${API_KEY}`
+          `/discover/tv/?with_network=213&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "trending",
         title: "Recomendados para você",
         items: await basicFetch(
-          `/trending/all/week?language=pt-BR&api_key=${API_KEY}`
+          `/trending/all/week?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "toprated",
         title: "Em Alta",
         items: await basicFetch(
-          `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`
+          `/movie/top_rated?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "comedy",
         title: "Comédia",
         items: await basicFetch(
-          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "horror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "romance",
         title: "Romance",
         items: await basicFetch(
-          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
       {
         slug: "documentary",
         title: "Documentários",
         items: await basicFetch(
-          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
         ),
       },
     ];
@@ -89,13 +86,13 @@ const exportedObjetc = {
       switch (type) {
         case "movie":
           info = await basicFetch(
-            `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            `/movie/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
           );
           break;
 
         case "tv":
           info = await basicFetch(
-            `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            `/tv/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_API_KEY}`
           );
           break;
         default:
